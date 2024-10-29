08a6abd1df256df839f764c7b4cb24c8f7ca7808
Julia Kanechnaya <jk@Julias-MacBook-Pro.local>
2024-10-28 21:04:32 -0500

    feat: userSignUp negative tests homework

--- src/data/user.ts
+++ src/data/user.ts
@@ -10,6 +10,9 @@ export const user = {
   "role": "admin"
 }
 
+export const enumValues = ["user", "guide", "lead-guide", "admin"]
+
+
 export function getUser() {
   let password = faker.internet.password()
   return {
@@ -31,4 +34,73 @@ export function createRandomUser() {
     birthdate: faker.date.birthdate(),
     registeredAt: faker.date.past(),
   };
+}
+
+export function getUserWithEmptyName() {
+  let password = faker.internet.password()
+  return {
+    "email": faker.internet.email(),
+    "password": password,
+    "passwordConfirm": password,
+    "role": "admin"
+  }
+}
+
+export function getUserWithEmptyEmail() {
+  let password = faker.internet.password()
+  return {
+    "name": faker.internet.userName(),
+    "password": password,
+    "passwordConfirm": password,
+    "role": "admin"
+  }
+}
+
+export function getUserWithEmptyPass() {
+  let password = faker.internet.password()
+  return {
+    "name": faker.internet.userName(),
+    "email": faker.internet.email(),
+    "passwordConfirm": password,
+    "role": "admin"
+  }
+}
+
+export function getUserWithEmptyComfirmPass() {
+  let password = faker.internet.password()
+  return {
+    "name": faker.internet.userName(),
+    "email": faker.internet.email(),
+    "password": password,
+    "role": "admin"
+  }
+}
+
+export function getUserWithBothPass() {
+  return {
+    "name": faker.internet.userName(),
+    "email": faker.internet.email(),
+    "role": "admin"
+  }
+}
+
+export function getUserWithNotMatchingComfirmPass() {
+  return {
+    "name": faker.internet.userName(),
+    "email": faker.internet.email(),
+    "password": faker.internet.password(),
+    "passwordConfirm": faker.internet.password(),
+    "role": "admin"
+  }
+}
+
+export function getUserWithUnexistingRole() {
+  let password = faker.internet.password()
+  return {
+    "name": faker.internet.userName(),
+    "email": faker.internet.email(),
+    "password": password,
+    "passwordConfirm": password,
+    "role": "maggle"
+  }
 }
--- src/specs/user/signUp.spec.ts
+++ src/specs/user/signUp.spec.ts
@@ -1,4 +1,15 @@
-import { getUser, user } from "../../data/user";
+import {
+  getUser,
+  user,
+  getUserWithEmptyComfirmPass,
+  getUserWithEmptyEmail,
+  getUserWithEmptyName,
+  getUserWithEmptyPass,
+  getUserWithNotMatchingComfirmPass,
+  getUserWithUnexistingRole,
+  getUserWithBothPass,
+  enumValues
+} from "../../data/user";
 import * as supertest from "supertest";
 const request = supertest("localhost:8001/api/v1");
 
@@ -12,7 +23,7 @@ describe("USER SIGNUP", () => {
       console.log(res.body, "response");
     });
 
-    it("Creat a new user using faker", async () => {
+    it("Creat a new user using faker with data as an obgect and async/await (jest)", async () => {
       const res = await request.post("/users/signup").send(user).expect(201);
       expect(res.body.data.user.name).toBe(user.name);
       expect(res.body.data.user.email).toBe(user.email.toLowerCase());
@@ -20,7 +31,7 @@ describe("USER SIGNUP", () => {
       console.log(res.body, "response");
     });
 
-    it.only("Creat a new user using faker", function (done) {
+    it("Creat a new user using faker with data as a function (supertest)", function (done) {
       let userImport = getUser();
       const res = request
         .post("/users/signup")
@@ -36,19 +47,322 @@ describe("USER SIGNUP", () => {
         });
     });
   });
-  describe("POSITIVE TESTING", () => {
-    it.only("Creat a new user", async () => {
-      const res = await request.post("/users/signup").send(
-        {
+  describe.only("NEGATIVE TESTING", () => {
+    it("Creat a new user with existing email", async () => {
+      const res = await request
+        .post("/users/signup")
+        .send({
           name: "Jane Doe",
           email: "jane.doe@gmail.com",
           password: "password123",
           passwordConfirm: "password123",
           role: "admin",
-        }
-      ).expect(500);
-      expect(res.body.message).toBe("E11000 duplicate key error collection: test.users index: email_1 dup key: { email: \"jane.doe@gmail.com\" }");
+        })
+        .expect(500);
+      expect(res.body.message).toBe(
+        'E11000 duplicate key error collection: test.users index: email_1 dup key: { email: "jane.doe@gmail.com" }'
+      );
       expect(res.body.status).toBe("error");
+      console.log(res.body, "response");
+    });
+
+    it("Creat a new user with empty name and async/await", async () => {
+      let importUserEmptyName = getUserWithEmptyName();
+      const res = await request
+        .post("/users/signup")
+        .send(importUserEmptyName)
+        .expect(500);
+      console.log(res.body.error, "response");
+      expect(res.body.message).toBe(
+        "User validation failed: name: Please tell us your name!"
+      );
+      expect(res.body.error.errors.name.name).toBe("ValidatorError");
+      expect(res.body.error.errors.name.message).toBe(
+        "Please tell us your name!"
+      );
+      expect(res.body.error._message).toBe("User validation failed");
+      expect(res.body.error.statusCode).toBe(500);
+      expect(res.body.error.errors.name.properties.message).toBe(
+        "Please tell us your name!"
+      );
+      expect(res.body.error.errors.name.properties.type).toBe("required");
+      expect(res.body.error.errors.name.properties.path).toBe("name");
+      expect(res.body.status).toBe("error");
+      console.log(res.body, "response");
+    });
+    it("Creat a new user with empty email and done", function (done) {
+      let importUserEmptyEmail = getUserWithEmptyEmail();
+      const res = request
+        .post("/users/signup")
+        .send(importUserEmptyEmail)
+        .expect(500)
+        .end(function (err, res) {
+          if (err) return done(err);
+          expect(res.body.status).toBe("error");
+          expect(res.body.message).toBe(
+            "User validation failed: email: Please provide your email"
+          );
+
+          //   expect(res.body.error._message).toBe("User validation failed");
+          //   expect(res.body.error.statusCode).toBe(500);
+          //   expect(res.body.error.status).toBe("error");
+          //   expect(res.body.error.name).toBe("ValidatorError");
+          //   expect(res.body.error.message).toBe(
+          //     "User validation failed: email: Please provide your email"
+          //   );
+          console.log(res.body.error.errors, "response");
+          return done();
+        });
+    });
+
+    it("Creat a new user with empty pass and async/await", async () => {
+      let importUserEmptyPass = getUserWithEmptyPass();
+      const res = await request
+        .post("/users/signup")
+        .send(importUserEmptyPass)
+        .expect(500);
+      console.log(res.body.error, "response");
+
+      expect(res.body.status).toBe("error");
+      expect(res.body.message).toBe(
+        "User validation failed: password: Please provide a password, passwordConfirm: Passwords are not the same!"
+      );
+
+      expect(res.body.error._message).toBe("User validation failed");
+      expect(res.body.error.statusCode).toBe(500);
+      expect(res.body.error.status).toBe("error");
+      expect(res.body.error.name).toBe("ValidationError");
+      expect(res.body.error.message).toBe(
+        "User validation failed: password: Please provide a password, passwordConfirm: Passwords are not the same!"
+      );
+
+      expect(res.body.error.errors.password.name).toBe("ValidatorError");
+      expect(res.body.error.errors.password.message).toBe(
+        "Please provide a password"
+      );
+      expect(res.body.error.errors.password.kind).toBe("required");
+      expect(res.body.error.errors.password.path).toBe("password");
+
+      expect(res.body.error.errors.password.properties.message).toBe(
+        "Please provide a password"
+      );
+      expect(res.body.error.errors.password.properties.type).toBe("required");
+      expect(res.body.error.errors.password.properties.path).toBe("password");
+
+      //
+      expect(res.body.error.errors.passwordConfirm.name).toBe("ValidatorError");
+      expect(res.body.error.errors.passwordConfirm.message).toBe(
+        "Passwords are not the same!"
+      );
+      expect(res.body.error.errors.passwordConfirm.kind).toBe("user defined");
+      expect(res.body.error.errors.passwordConfirm.path).toBe(
+        "passwordConfirm"
+      );
+      expect(res.body.error.errors.passwordConfirm.value).toBe(
+        importUserEmptyPass.passwordConfirm
+      );
+
+      expect(res.body.error.errors.passwordConfirm.properties.message).toBe(
+        "Passwords are not the same!"
+      );
+      expect(res.body.error.errors.passwordConfirm.properties.type).toBe(
+        "user defined"
+      );
+      expect(res.body.error.errors.passwordConfirm.properties.path).toBe(
+        "passwordConfirm"
+      );
+      expect(res.body.error.errors.passwordConfirm.properties.value).toBe(
+        importUserEmptyPass.passwordConfirm
+      );
+
+      console.log(res.body, "response");
+    });
+
+    it("Creat a new user with empty confirm pass and async/await", async () => {
+      let importUserEmptyConfPass = getUserWithEmptyComfirmPass();
+      const res = await request
+        .post("/users/signup")
+        .send(importUserEmptyConfPass)
+        .expect(500);
+      console.log(res.body.error, "response");
+
+      expect(res.body.status).toBe("error");
+      expect(res.body.message).toBe(
+        "User validation failed: passwordConfirm: Please confirm your password"
+      );
+
+      expect(res.body.error._message).toBe("User validation failed");
+      expect(res.body.error.statusCode).toBe(500);
+      expect(res.body.error.status).toBe("error");
+      expect(res.body.error.name).toBe("ValidationError");
+      expect(res.body.error.message).toBe(
+        "User validation failed: passwordConfirm: Please confirm your password"
+      );
+
+      //
+      expect(res.body.error.errors.passwordConfirm.name).toBe("ValidatorError");
+      expect(res.body.error.errors.passwordConfirm.message).toBe(
+        "Please confirm your password"
+      );
+      expect(res.body.error.errors.passwordConfirm.kind).toBe("required");
+      expect(res.body.error.errors.passwordConfirm.path).toBe(
+        "passwordConfirm"
+      );
+
+      expect(res.body.error.errors.passwordConfirm.properties.message).toBe(
+        "Please confirm your password"
+      );
+      expect(res.body.error.errors.passwordConfirm.properties.type).toBe(
+        "required"
+      );
+      expect(res.body.error.errors.passwordConfirm.properties.path).toBe(
+        "passwordConfirm"
+      );
+
+      console.log(res.body, "response");
+    });
+
+    it("Creat a new user with BOTH empty pass and async/await", async () => {
+      let importUserBothEmptyPass = getUserWithBothPass();
+      const res = await request
+        .post("/users/signup")
+        .send(importUserBothEmptyPass)
+        .expect(500);
+      console.log(res.body.error, "response");
+
+      expect(res.body.status).toBe("error");
+      expect(res.body.message).toBe(
+        "User validation failed: password: Please provide a password, passwordConfirm: Please confirm your password"
+      );
+
+      expect(res.body.error._message).toBe("User validation failed");
+      expect(res.body.error.statusCode).toBe(500);
+      expect(res.body.error.status).toBe("error");
+      expect(res.body.error.name).toBe("ValidationError");
+      expect(res.body.error.message).toBe(
+        "User validation failed: password: Please provide a password, passwordConfirm: Please confirm your password"
+      );
+
+      expect(res.body.error.errors.password.name).toBe("ValidatorError");
+      expect(res.body.error.errors.password.message).toBe(
+        "Please provide a password"
+      );
+      expect(res.body.error.errors.password.kind).toBe("required");
+      expect(res.body.error.errors.password.path).toBe("password");
+
+      expect(res.body.error.errors.password.properties.message).toBe(
+        "Please provide a password"
+      );
+      expect(res.body.error.errors.password.properties.type).toBe("required");
+      expect(res.body.error.errors.password.properties.path).toBe("password");
+
+      //
+      expect(res.body.error.errors.passwordConfirm.name).toBe("ValidatorError");
+      expect(res.body.error.errors.passwordConfirm.message).toBe(
+        "Please confirm your password"
+      );
+      expect(res.body.error.errors.passwordConfirm.kind).toBe("required");
+      expect(res.body.error.errors.passwordConfirm.path).toBe(
+        "passwordConfirm"
+      );
+
+      expect(res.body.error.errors.passwordConfirm.properties.message).toBe(
+        "Please confirm your password"
+      );
+      expect(res.body.error.errors.passwordConfirm.properties.type).toBe(
+        "required"
+      );
+      expect(res.body.error.errors.passwordConfirm.properties.path).toBe(
+        "passwordConfirm"
+      );
+
+      console.log(res.body, "response");
+    });
+    it("Creat a new user with not matching passes and async/await", async () => {
+      let importUserNotMatchPass = getUserWithNotMatchingComfirmPass();
+      const res = await request
+        .post("/users/signup")
+        .send(importUserNotMatchPass)
+        .expect(500);
+      console.log(res.body.error, "response");
+
+      expect(res.body.status).toBe("error");
+      expect(res.body.message).toBe(
+        "User validation failed: passwordConfirm: Passwords are not the same!"
+      );
+
+      expect(res.body.error._message).toBe("User validation failed");
+      expect(res.body.error.statusCode).toBe(500);
+      expect(res.body.error.status).toBe("error");
+      expect(res.body.error.name).toBe("ValidationError");
+      expect(res.body.error.message).toBe(
+        "User validation failed: passwordConfirm: Passwords are not the same!"
+      );
+
+      //
+      expect(res.body.error.errors.passwordConfirm.name).toBe("ValidatorError");
+      expect(res.body.error.errors.passwordConfirm.message).toBe(
+        "Passwords are not the same!"
+      );
+      expect(res.body.error.errors.passwordConfirm.kind).toBe("user defined");
+      expect(res.body.error.errors.passwordConfirm.path).toBe(
+        "passwordConfirm"
+      );
+      expect(res.body.error.errors.passwordConfirm.value).toBe(
+        importUserNotMatchPass.passwordConfirm
+      );
+
+      expect(res.body.error.errors.passwordConfirm.properties.message).toBe(
+        "Passwords are not the same!"
+      );
+      expect(res.body.error.errors.passwordConfirm.properties.type).toBe(
+        "user defined"
+      );
+      expect(res.body.error.errors.passwordConfirm.properties.path).toBe(
+        "passwordConfirm"
+      );
+      expect(res.body.error.errors.passwordConfirm.properties.value).toBe(
+        importUserNotMatchPass.passwordConfirm
+      );
+
+      console.log(res.body, "response");
+    });
+    it.only("Creat a new user with non-existing role and async/await", async () => {
+      let importUserUnexistRole = getUserWithUnexistingRole();
+      const res = await request
+        .post("/users/signup")
+        .send(importUserUnexistRole)
+        .expect(500);
+      console.log(res.body.error, "response");
+      expect(res.body.status).toBe("error");
+      expect(res.body.message).toBe(
+        "User validation failed: role: `" + importUserUnexistRole.role + "` is not a valid enum value for path `role`."
+      );
+    //   expect(res.body.error.errors.name.name).toBe("ValidatorError");
+    //   expect(res.body.error.errors.name.message).toBe(
+    //     "Please tell us your name!"
+    //   );
+      expect(res.body.error._message).toBe("User validation failed");
+      expect(res.body.error.statusCode).toBe(500);
+      expect(res.body.error.name).toBe("ValidationError");
+      expect(res.body.error.message).toBe("User validation failed: role: `" + importUserUnexistRole.role + "` is not a valid enum value for path `role`.");
+
+      expect(res.body.error.errors.role.name).toBe(
+        "ValidatorError"
+      );
+      expect(res.body.error.errors.role.message).toBe("`" + importUserUnexistRole.role + "` is not a valid enum value for path `role`.");
+      expect(res.body.error.errors.role.kind).toBe("enum");
+      expect(res.body.error.errors.role.path).toBe("role");
+      expect(res.body.error.errors.role.value).toBe(importUserUnexistRole.role);
+      
+      expect(res.body.error.errors.role.properties.message).toBe(
+        "`" + importUserUnexistRole.role + "` is not a valid enum value for path `role`."
+      );
+      expect(res.body.error.errors.role.properties.type).toBe("enum");
+      expect(res.body.error.errors.role.properties.path).toBe("role");
+      expect(res.body.error.errors.role.properties.value).toBe(importUserUnexistRole.role);
+      expect(res.body.error.errors.role.properties.enumValues).toEqual(enumValues);
+      
       console.log(res.body, "response");
     });
   });
